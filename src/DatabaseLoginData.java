2132f819a82df6d806c3afd2b7844f336db34caf
SuneLindmark <sune.lindmark@elev.ga.ntig.se>
2020-03-24 09:52:46 +0100

    databas

---
+++ src/DatabaseLoginData.java
@@ -0,0 +1,8 @@
+public class DatabaseLoginData {
+        public static final String DBURL = "localhost";
+        public static String port = "3306";
+        public static String DBname = "soloadventure";
+        public static String user = "SuneBune";
+        public static String password = "hej123";
+    }
+
---
+++ src/database.java
@@ -0,0 +1,40 @@
+
+import java.sql.*;
+
+public class database {
+    public static void main(String[] args) {
+        try {
+            // Set up connection to database
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://"+DatabaseLoginData.DBURL + ":" + DatabaseLoginData.port + "/" + DatabaseLoginData.DBname +
+                            "? allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                    DatabaseLoginData.user, DatabaseLoginData.password);
+
+            // Setup statement
+            Statement stmt = conn.createStatement();
+            // Create query and execute
+            int x = 1;
+            String strSelect = "select body from story where id = " + x;
+            System.out.println("The SQL statement is: " + strSelect + "\n");
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Loop through the result set and print
+
+            System.out.println("The records selected are:");
+            int rowCount = 0;
+            while(rset.next()) {
+                String title = rset.getString("body");
+                System.out.println(title);
+                ++rowCount;
+            }
+            System.out.println("Total number of records = " + rowCount);
+
+            // Close conn and stmt
+            conn.close();
+            stmt.close();
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
